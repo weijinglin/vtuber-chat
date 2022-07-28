ad0e94f90d37b49ea626f9d2f3b4bf14da6f5e68
weijinglin <weijinglin@sjtu.edu.cn>
2022-07-28 10:58:50 +0800

    add style.css and add vtubView

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {Cameraview} from "./view/cameraview";
+import {VtubchatView} from "./view/VtubchatView";
 
 function App() {
   return (
     //
-      <Cameraview></Cameraview>
+    //   <Cameraview></Cameraview>
+      <VtubchatView></VtubchatView>
   );
 }
 
