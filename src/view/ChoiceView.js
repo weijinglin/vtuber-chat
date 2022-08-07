a647fb04a6ba04c3abf05a0ca3d4f85302da002e
weijinglin <weijinglin@sjtu.edu.cn>
2022-08-07 15:57:42 +0800

    success to add router and success to build chat between vtuber and normal camera

--- src/view/ChoiceView.js
+++ src/view/ChoiceView.js
@@ -1,17 +1,27 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {Button} from "antd";
 
 export function ChoiceView(props) {
 
+    const navigate = useNavigate();
+
+    const simpleHandle = () => {
+        navigate("/simple");
+    }
+
+    const vtubHandle = () => {
+        navigate("/vtuber");
+    }
+
     return(
         <div>
-            <div>
-                <Button className="choice">
+            <div className="buttondiv">
+                <Button className="choice" onClick={simpleHandle}>
                     simple-chat
                 </Button>
             </div>
-            <div>
-                <Button className="choice">
+            <div className="buttondiv">
+                <Button className="choice" onClick={vtubHandle}>
                     vtub-chat
                 </Button>
             </div>
