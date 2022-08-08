45e469584844c3d9705ead8efabb0dc1dc9dafe9
weijinglin <weijinglin@sjtu.edu.cn>
2022-08-08 22:42:09 +0800

    add urlcache

---
+++ src/cache/Urlcache.js
@@ -0,0 +1,2 @@
+export const datacache = ["./models/hiyori/hiyori_pro_t10.model3.json","./models/haru_greeter_pro_jp/runtime/haru_greeter_t03.model3.json"
+,"./models/mao_pro_zh/runtime/mao_pro_t02.model3.json"];
